8d35dd4f34340ce2f81bc2c55c19453556035266
Danny <90986300+djecodes@users.noreply.github.com>
2022-11-07 15:27:59 -0600

    Update basic.js

--- Basic JavaScript/basic.js
+++ Basic JavaScript/basic.js
@@ -443,3 +443,22 @@ console.log("After: " + JSON.stringify(testArr));
 function welcomeToBooleans() {
     return true;
 }
+
+/* if statements are used to make decisions in code. The keyword 'if' tells JS to execute
+** the code in the curly braces under certain conditions, defined in the parentheses. These
+** conditions are known as Boolean conditions and they may only be true or false.
+**
+** When the condition evaluates to true, the program executes the statement inside the curly
+** braces.
+** When the condition evaluates to false, the statement inside the curly braces will not
+** execute.
+*/
+/* Create an if statement inside the function to return "Yes, that was true" if the parameter
+** wasThatTrue is true and return "No, that was false" otherwise.
+*/
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue == true) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+}
