f106d6d60b5e4dc8bb52c3eb46775253afca5db8
Danny <90986300+djecodes@users.noreply.github.com>
2022-11-11 17:23:03 -0600

    Update basic.js

--- Basic JavaScript/basic.js
+++ Basic JavaScript/basic.js
@@ -799,3 +799,166 @@ function switchOfStuff(val){
 }
 
 switchOfStuff(1);
+
+/* If the break statement is omitted from a switch statement's case, the following case statement(s)
+ * are executed until a break is encountered.
+ */
+/* Write a switch statement to set answer for the following ranges:
+ * 1-3 => low
+ * 4-6 => Mid
+ * 7-9 => High
+ */
+function sequentialSizes(val) {
+    let answer = "";
+
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+    }
+
+    return answer;
+}
+
+sequentialSizes(1);
+
+// If you have many options to choose from, a switch statement can be easier than many chained if/else statements
+// Change the chained if/else if statements into a switch statement
+function chainToSwitch(val) {
+    let answer = "";
+
+    switch(val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+
+    return answer;
+}
+
+chainToSwitch(7);
+
+// We can return the result of a comparison without an if/else because === returns true or false.
+// Fix the function isLess to remove the if/else statements
+
+/* Previous code: 
+ * function isLess(a, b) {
+ *    if (a < b) {
+ *      return true;
+ * } else {
+ *      return false;
+ * }
+ */
+function isLess(a, b){
+    return a < b;
+}
+
+isLess(10, 15);
+
+/* When a return statement is reached, the execution of the current function stops and control returns
+ * to the calling location.
+ */
+/* Modify the function abTest so that if a or b are less than 0 the function will immediately exit with
+ * a value of undefined
+ */
+function abTest(a, b) {
+    if (a < 0 || b < 0){
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
+/* Counting Cards Challenge:
+ * 
+ * In the casino game Blackjack, a player can determine whether they have an advantage on the next hand
+ * over the house by keeping track of the relative number of high and low cards remaining in the deck.
+ * This is called Card Counting.
+ * 
+ * Having more high cards remaining in the deck favors the player. Each card is assigned a value according
+ * to the table below. When the count is positive, the player should bet high. When the count is zero or
+ * negative, the player should bet low.
+ * 
+ * Count Change - Cards
+ * + 1 - 2, 3, 4, 5, 6
+ * 0 - 7, 8, 9
+ * -1 - 10, 'J', 'Q', 'K', 'A'
+ * 
+ * Instructions
+ * 
+ * You will write a card counting function. It will receive a 'card' parameter, which can be a number or
+ * a string, and increment or decrement the global 'count' variable according to the card's value.
+ * The function will then return a string with the current count and the string 'Bet' if the count is 
+ * positive, or 'Hold' if the count is zero or negative. The current count and the player's decision
+ * (Bet or Hold) should be separated by a single space.
+ * 
+ * Example Outputs: "-3 Hold" or "5 Bet"
+ */
+let count = 0;
+
+function cc(card) {
+
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count += 1;
+            break;
+        case 7:
+        case 8:
+        case 9:
+            count += 0;
+            break;
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            count -= 1;
+            break;
+    }
+
+    if (count > 0) {
+        return count + " " + "Bet";
+    } else {
+        return count + " " + "Hold";
+    }
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// Objects are useful for storing data, through properties, and can represent real world objects, like a cat
+// Make an object that represents a dog called myDog
+const myDog = {
+    "name": "Princess",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Lucy", "Oats"]
+};
