a0bc9f8447eda7e345847fbbdb2bde271de166a8
Danny <90986300+djecodes@users.noreply.github.com>
2022-11-11 15:04:11 -0600

    Update basic.js

--- Basic JavaScript/basic.js
+++ Basic JavaScript/basic.js
@@ -735,3 +735,67 @@ function golfScore(par, strokes) {
 }
 
 golfScore(5, 4);
+
+/* A switch statement tests a value and can have many case statements which define various possible values
+ * Statements are executed from the first matched case value until a break is encountered.
+ */
+/* Write a switch statement which test val and sets answer for the following conditions:
+ * 1 - alpha
+ * 2 - beta
+ * 3 - gamma
+ * 4 - delta
+ */
+function caseInSwitch(val){
+    let answer = "";
+
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+
+    return answer;
+}
+
+caseInSwitch(1);
+
+/* In a switch statement you may not be able to specify all possible values as case statements.
+ * Instead, you can add the default statement which will executed if no matching case are found.
+ */
+/* Write a switch statement to set answer for the following conditions:
+ * a - apple
+ * b - bird
+ * c - cat
+ * default - stuff
+ */
+function switchOfStuff(val){
+    let answer = "";
+
+    switch(val) {
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+            break;
+    }
+
+    return answer;
+}
+
+switchOfStuff(1);
