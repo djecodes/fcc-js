b87d55da3e17fefd902255ccf913bd96b3bb1de9
Danny <90986300+djecodes@users.noreply.github.com>
2022-11-20 22:27:31 -0600

    Update es6.js

--- ES6/es6.js
+++ ES6/es6.js
@@ -1,1 +1,15 @@
-// placeholder for future solutions
+/* When you declare a variable with the let keyword inside a block, statement, or expression,
+ * its scope is limited to that block, statement, or expression.
+ *
+ * Fix the code so that 'i' declared in the if statement is a separate variable than 'i' declared
+ * in the first line of the function.
+ */
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+        let i = 'block scope';
+        console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope is: ', i);
+    return i;
+}
