b2be03a6e06077074ddbdaec7240d631fa486885
Danny <90986300+djecodes@users.noreply.github.com>
2022-11-21 19:49:28 -0600

    Update es6.

--- ES6/es6.js
+++ ES6/es6.js
@@ -13,3 +13,20 @@ function checkScope() {
     console.log('Function scope is: ', i);
     return i;
 }
+
+/* Some developers prefer to assign all their variables using const by default, unless they know
+ * they will need to reassign the value. Only in that case, they use let.
+ * 
+ * However, it is important to understand that objects (including arrays and functions) assigned
+ * to a variable using const are still mutable. Using the const declaration only prevents reassign-
+ * ment of the variable identifier.
+ * 
+ * Change the array to [2, 5, 7] using various element assignments.
+ */
+const s = [5, 7, 2];
+function editInPlace() {
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+}
+editInPlace();
