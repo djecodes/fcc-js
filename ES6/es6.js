e00e0a4a540d45d344b1b6f4be424ed788be48ab
Danny <90986300+djecodes@users.noreply.github.com>
2022-11-22 11:25:15 -0600

    Update es6.

--- ES6/es6.js
+++ ES6/es6.js
@@ -30,3 +30,28 @@ function editInPlace() {
     s[2] = 7;
 }
 editInPlace();
+
+/* const declaration alone doesn't really protect your data from mutation. To ensure your data doesn't
+ * change, JS provides a function Object.freeze to prevent data mutation.
+ * 
+ * Any attempt at changing the object will be rejected, with an error thrown if the script is running
+ * in strict mode.
+ * 
+ * In this challenge, you need to freeze the MATH_CONSTANTS object so that no one is able to alter the
+ * value of PI, add, or delete properties.
+ */
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+
+    Object.freeze(MATH_CONSTANTS);
+
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
