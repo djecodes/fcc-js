b726f52726dc43ca1653192582b7725d71846545
Danny <90986300+djecodes@users.noreply.github.com>
2022-11-27 16:51:01 -0600

    Update es6.js

--- ES6/es6.js
+++ ES6/es6.js
@@ -262,3 +262,28 @@ const bicycle = {
 
 bicycle.setGear(3);
 console.log(bicycle.gear);
+
+/* ES6 provides a new syntax to create objects, using the class keyword.
+ * 
+ * In ES6, a class declaration has a constructor method that is invoked with the new keyword.
+ * 
+ * If the constructor method is not explicitly defined, then it is implicity defined with no arguments.
+ * 
+ * It should be noted that the class keyword declares a new function, to which a constructor is added.
+ * 
+ * This constructor is invoked when new is called to create a new object.
+ * 
+ * Note: UpperCamelCase should be used by convention for ES6 class names.
+ * 
+ * The constructor method is a special method for creating and initializing an object created with a class.
+ * 
+ * Use the class keyword and write a constructor to create the Vegetable class.
+ */
+class Vegetable {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
