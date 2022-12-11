46951419260c95733e7bc493eb355f385c5b737a
Danny <90986300+djecodes@users.noreply.github.com>
2022-12-10 21:45:04 -0600

    Update regex.js

--- Regular Expressions/regex.js
+++ Regular Expressions/regex.js
@@ -212,3 +212,18 @@ let result = calRegex.test(rickyAndCal);
 let caboose = "The last car on a train is the caboose";
 let lastRegex = /caboose$/;
 let result = lastRegex.test(caboose);
+
+/* Using character classes, you were able to search for all letters of the alphabet with [a-z]. This kind
+ * of character class is common enough that there is a shortcut for it, although it includes a few extra
+ * characters as well.
+ * 
+ * The closest character class in JS to match the alphabet is /w. This shortcut is equal to [A-Za-z0-9_].
+ * This character class matches upper and lowercase letters plus numbers. Note, this character class also
+ * includes the underscore character (_).
+ * 
+ * Use the shorthand character class /w to count the number of alphanumeric characters in various quotes
+ * and strings.
+ */
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g;
+let result = quoteSample.match(alphabetRegexV2).length;
