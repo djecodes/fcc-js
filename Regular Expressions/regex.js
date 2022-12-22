edc5bca128681caa90814bc2e94f8be737438dac
Danny <90986300+djecodes@users.noreply.github.com>
2022-12-22 16:13:03 -0600

    Update regex.js

--- Regular Expressions/regex.js
+++ Regular Expressions/regex.js
@@ -353,3 +353,86 @@ let result = timRegex.test(timStr);
 let favWord = "favorite";
 let favRegex = /favou?rite/;
 let result = favRegex.test(favWord);
+
+/* Lookaheads are patterns that tell JS to look-ahead in your string to check for patterns further along. 
+ * This can be useful when you want to search for multiple patterns over the same string.
+ *
+ * There are two kinds of lookaheads:
+ * Positive lookahead will look to make sure the element in the search pattern is there, but won't actually 
+ * match it. A positive lookahead is used as (?=...) where the ... is the required part that is not matched.
+ * 
+ * Negative lookahead will look to make sure the element in the search pattern is not there. A negative lookahead
+ * is used as (?!...) where the ... is the pattern that you do not want to be there. The rest of the pattern is
+ * returned if the negative lookahead part is not present.
+ * 
+ * Examples of lookaheads:
+ * 
+ * let quRegex= /q(?=u)/; //positive lookahead
+ * let gRegex = /q(?!u)/; //negative lookahead
+ * 
+ * Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two
+ * consecutive digits.
+ */
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6})(?=\w*\d{2})/;
+let result = pwRegex.test(sampleWord);
+
+/* Sometimes we want to check for groups of characters using a Regular Expression and to achieve that we use parentheses.
+ * 
+ * Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case sensitive manner and
+ * it should make concessions for middle name.
+ * 
+ * Then fix the code so that the regex that you have created is checked against myString and either true or false is returned
+ * depending on whether the regex matches.
+ */
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin D.|Eleanor) Roosevelt/;
+let result = myRegex.test(myString);
+
+/* Capture groups are constructed by enclosing the regex pattern to be captured in parentheses. 
+ *
+ * The substring matched by the group is saved to a temporary "variable", which can be accessed within the same regex
+ * using a backslash and the number of the capture group (e.g. \1). Capture groups are automatically numbered by the
+ * position of their opening parentheses (left to right), starting at 1.
+ * 
+ * The example below matches a word that occurs thrice separated by spaces:
+ * let repeatRegex = /(\w+) \1 \1/;
+ * 
+ * Use capture groups in the reRegex to match a string that consist of only the same number repeated exactly three times
+ * separated by single spaces.
+ */
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+) \1 \1$/;
+let result = reRegex.test(repeatNum);
+
+/* You can search and replace text in a string using .replace() on a string. The inputs for .replace() is first the regex
+ * pattern you want to search for. The second parameter is the string to replace the match or a function to do something.
+ *
+ * let wrongText = "The sky is silver";
+ * let silverRegex = /silver/;
+ * wrongText.replace(silverRegex, "blue"); 
+ * 
+ * You can also access capture groups in the replacement string with dollar signs ($)
+ * 
+ * "Code Camp".replace(/(\w+)\s(\w+)/, '$2 $1');
+ * 
+ * 1.) Write a regex fixRegex using three capture groups that will search for each word in the string one two three. 
+ * 2.) Then update the replaceText variable to replace one two three with the string three two one and assign the result to the result variable. 
+ * 3.) Make sure you are utilizing capture groups in the replacement string using the dollar sign ($) syntax.
+ */
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/;
+let replaceText = "$3 $2 $1"; 
+let result = str.replace(fixRegex, replaceText);
+
+/* Sometimes whitespace characters around strings are not wanted but are there.
+ * Typical processing of strings is to remove the whitespace at the start and end of it.
+ *
+ * 
+ * Write a regex and use the appropriate string methods to remove whitespace at the beginning
+ * and end of strings.
+ */
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; 
+let result = hello.replace(wsRegex, "");
